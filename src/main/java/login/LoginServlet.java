bba5a7af804c13a3f35bc309b1591ae47680feea
darren88881 <darren88881@outlook.com>
2020-08-04 22:45:48 +0800

    解决相应乱码

--- src/main/java/login/LoginServlet.java
+++ src/main/java/login/LoginServlet.java
@@ -1,11 +1,16 @@
 package login;
 
+import bean.User;
+import dao.UserDao;
+import dao.UserDaoImpl;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 /**
  * @author darren
@@ -13,18 +18,28 @@ import java.io.PrintWriter;
  */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
+
+    private UserDao userDao = new UserDaoImpl();
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        PrintWriter writer = response.getWriter();
 
-        if(username.equals("") || password.equals("")){
-            writer.print("username and password is null");
-        }else{
-            writer.print("username and password not is null");
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = response.getWriter();
+        try {
+            User user = userDao.findUsername(username, password);
+            if(user==null){
+                writer.print("用户名或密码错误");
+            }else{
+                writer.print("登录成功");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
        writer.close();
-        System.out.println("post方法请求");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
